3727b70b3b3f1a7244fecf423960058a655c080f
yankelevich-dima <yankelevich.dima@gmail.com>
2016-09-08 22:26:53 +0300

    Added script and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 9_github_trending
+
+## Usage
+
+  - Run `python github_trending.py`
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,33 @@
+import requests
+
+TOP_COUNT = 20
+API_URL = 'https://api.github.com/search/repositories?q=created:>2016-08-25&sort=stars&order=desc'
 
 
 def get_trending_repositories(top_size):
-    pass
+    responce = requests.get(API_URL)
+    data = responce.json()
+    result = data['items'][:top_size]
+    return result
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def print_trending_repositories(repositories):
+    output = '''    Name: {name}
+    Stars: {stars}
+    Issues: {issues}
+    Url: {url}
+    '''
+    output += '*' * 75
+
+    for repo in repositories:
+        print(output.format(
+            name=repo['name'],
+            stars=repo['stargazers_count'],
+            issues=repo['open_issues_count'],
+            url=repo['html_url'],
+        ))
+
 
 if __name__ == '__main__':
-    pass
+    repositories = get_trending_repositories(TOP_COUNT)
+    print_trending_repositories(repositories)
